00315969bd1eab047eb44373fbec3cd8fec447ea
youjunjer <40359899+youjunjer@users.noreply.github.com>
2022-06-16 22:54:35 +0800

    Update 01Terminal_HelloWorld.ino

--- ESP32SampleCode/01Terminal_HelloWorld.ino
+++ ESP32SampleCode/01Terminal_HelloWorld.ino
@@ -1,4 +1,5 @@
-//This code will send BT string per second
+//這個程式將會對配對的裝置(Android)傳送藍芽訊息一秒一次，請使用APP的Terminal 功能接收ESP32的訊息
+//This code will send BT string per second to Android, please use APP Terminal function to receive ESP32 message
 #include <BluetoothSerial.h>
 BluetoothSerial BT;
 
