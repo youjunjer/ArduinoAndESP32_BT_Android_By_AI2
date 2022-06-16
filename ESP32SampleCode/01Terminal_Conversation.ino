e1a3fdca11f578b3c26a6bf4685565eb72e3c606
youjunjer <40359899+youjunjer@users.noreply.github.com>
2022-06-16 23:01:10 +0800

    Create 01Terminal_Conversation.ino

---
+++ ESP32SampleCode/01Terminal_Conversation.ino
@@ -0,0 +1,23 @@
+//這個程式將可以讓ESP32與Android進行雙向對話，請使用Terminal功能進行測試。
+//This code let you make text conversation between ESP32 and Android, please use APP Terminal function.
+#include <BluetoothSerial.h>
+BluetoothSerial SerialBT;
+
+void setup(){
+  Serial.begin(115200);
+  SerialBT.begin("Your ESP32 BT Name");//請將這裡改成你喜歡的藍芽名稱
+}
+
+void loop(){
+  //ESP32->Android
+  if(Serial.available())  {
+    String data=Serial.readString();
+    SerialBT.println(data);
+  } 
+  //Android->ESP32
+  if(SerialBT.available())  {
+    String data=SerialBT.readString();
+    Serial.println(data);
+  }
+  delay(1);
+}
